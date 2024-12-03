34057b7ca24164693bd37c1e053ecadec82f9071
Arnau Garcia Marti <arnaugarciaimarti@gmail.com>
2024-12-04 00:05:07 +0100

    Location Finder

--- BackEnd/Location_Finder.py
+++ BackEnd/Location_Finder.py
@@ -2,26 +2,39 @@ import json
 import os
 import requests
 import utm
+import time
 
 def get_location_info(lat, lon):
     api_key = 'd5759aade0884056ba8c63a7fe5d9f2f'
-    url = f'https://api.opencagedata.com/geocode/v1/json?q={lat}%2C{lon}&key={api_key}'
+    url1 = f'https://api.opencagedata.com/geocode/v1/json?q={lat}%2C{lon}&key={api_key}'
 
-    response = requests.get(url)
+    response1 = requests.get(url1)
 
-    if response.status_code == 200:
-        data = response.json()
-        if data['results']:
-            components = data['results'][0]['components']
-            direction = data['results'][0].get('formatted', 'N/A')
+    if response1.status_code == 200:
+        data1 = response1.json()
+        if data1['results']:
+            components = data1['results'][0]['components']
+            direction = data1['results'][0].get('formatted', 'N/A')
             postal_code = components.get('postcode', 'N/A')
+            time.sleep(5)  # Add a timeout before checking the postal code
+
             if postal_code == 'N/A':
-                print(f"Missing postcode for {lat}, {lon}: {json.dumps(data, indent=4)}")
+                print(f"Missing postcode for {lat}, {lon}: {json.dumps(data1, indent=4)}")
+                api2_key = '3b3eb29261ae4a61bd9fc55c2e50f74d'
+                url2 = f'https://api.geoapify.com/v1/geocode/reverse?lat={lat}&lon={lon}&apiKey={api2_key}'
+                response2 = requests.get(url2)
+
+                if response2.status_code == 200:
+                    data2 = response2.json()
+                    print(f"Response from second API: {json.dumps(data2, indent=4)}")  # Print the entire response from the second API
+                    if data2['results']:
+                        postal_code = data2['results'][0].get('postcode', 'N/A')
+                        print(postal_code)
             return direction, postal_code
         else:
             return 'N/A', 'N/A'
     else:
-        print(f"Error: {response.status_code}")
+        print(f"Error: {response1.status_code}")
         return 'N/A', 'N/A'
 
 def process_json(json_path):
