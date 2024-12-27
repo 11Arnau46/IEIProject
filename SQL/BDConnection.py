20c8dba1b13e83f4f00ce719ccfcb02943112c37
Shiyi <scheng1@upv.edu.es>
2024-12-27 23:42:03 +0100

    Borra la base de datos si existe, y vuelve a crearla

--- SQL/BDConnection.py
+++ SQL/BDConnection.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import OperationalError
 from .BDMap import Base  # Importación relativa al paquete actual
@@ -21,14 +21,25 @@ class BDConnection:
         """ Crea un motor de SQLAlchemy con la base de datos especificada """
         return create_engine(f'mysql+mysqlconnector://{self.usuario}:{self.contrasena}@{self.host}:{self.puerto}/{self.db_name}', echo=False)
 
+    def drop_database_if_exists(self):
+        """ Elimina la base de datos si existe """
+        try:
+            engine_no_db = self.create_engine_without_db()
+            with engine_no_db.connect() as conn:
+                conn.execute(text(f"DROP DATABASE IF EXISTS {self.db_name}"))
+                conn.commit()
+        except OperationalError as e:
+            print(f"Error al intentar eliminar la base de datos: {e}")
+            raise
+
     def create_database_if_not_exists(self):
         """ Crear la base de datos si no existe """
         try:
             # Usamos el motor sin base de datos especificada para verificar la existencia
             engine_no_db = self.create_engine_without_db()
             with engine_no_db.connect() as conn:
-                conn.connection.cursor().execute(f"CREATE DATABASE IF NOT EXISTS {self.db_name}")
-                print(f"Base de datos '{self.db_name}' creada o ya existente.")
+                conn.execute(text(f"CREATE DATABASE IF NOT EXISTS {self.db_name}"))
+                conn.commit()
         except OperationalError as e:
             print(f"Error al intentar conectar con la base de datos: {e}")
             raise
@@ -36,13 +47,16 @@ class BDConnection:
     def init_db(self):
         """ Inicializa la base de datos y crea las tablas si es necesario """
         try:
-            # Primero, asegurarse de que la base de datos exista
+            # Primero, eliminar la base de datos si existe
+            self.drop_database_if_exists()
+            
+            # Luego, crear la base de datos
             self.create_database_if_not_exists()
 
             # Ahora creamos el motor conectado a la base de datos
             self.engine = self.create_engine_with_db()
 
-            # Crear las tablas si no existen
+            # Crear las tablas
             Base.metadata.create_all(self.engine)
             print("Tablas creadas exitosamente.")
             
